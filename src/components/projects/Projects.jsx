22fcb28244efecd95ddd83089173c4f31dbf2814
Justin Louckx <jlouckx@gmail.com>
2022-09-26 18:34:59 +0200

    mobile-references half done - needs styling

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -102,86 +102,85 @@ export default function Projects() {
           </>
         )}
       </Mobile>
-      <Desktop>
-        <h1>Projects</h1>
-        <div
-          className="slider"
-          style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
-        >
-          {data.map((item) => (
-            <div className="container">
-              {!gif ? (
-                <>
-                  <div className="item">
-                    <div className="left">
-                      <div className="leftContainer">
-                        <h2>{item.name}</h2>
-                        <p>{item.description}</p>
-                        <div className="featuresContainer">
-                          <span>Features</span>
-                          <ul className="features">
-                            {item.features.map((feature) => (
-                              <li>{feature}</li>
-                            ))}
-                          </ul>
-                        </div>
 
-                        <div className="links"></div>
+      <h1>Projects</h1>
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((item) => (
+          <div className="container">
+            {!gif ? (
+              <>
+                <div className="item">
+                  <div className="left">
+                    <div className="leftContainer">
+                      <h2>{item.name}</h2>
+                      <p>{item.description}</p>
+                      <div className="featuresContainer">
+                        <span>Features</span>
+                        <ul className="features">
+                          {item.features.map((feature) => (
+                            <li>{feature}</li>
+                          ))}
+                        </ul>
                       </div>
+
+                      <div className="links"></div>
                     </div>
-                    <div className="right">
-                      <img className="mainImage" src={item.source} alt="" />
+                  </div>
+                  <div className="right">
+                    <img className="mainImage" src={item.source} alt="" />
 
-                      <div className="stacksContainer">
-                        {item.stack.map((item) => (
-                          // console.log(item)
-                          <div className="stacks">
-                            <img
-                              className="stacksImg"
-                              src={item.source}
-                              alt=""
-                              key={item.source}
-                            ></img>
-                            <span className="name">{item.name}</span>
-                          </div>
-                        ))}
-                      </div>
-
-                      <div className="links">
-                        <a href={item.github} target="_blank">
-                          <GitHub className="icon" />
-                        </a>
-                        <a href={item.live} target="_blank">
-                          <Laptop className="icon" />
-                        </a>
-                        <div className="gif" onClick={() => setGif(true)}>
-                          <GifBox className="icon" />
+                    <div className="stacksContainer">
+                      {item.stack.map((item) => (
+                        // console.log(item)
+                        <div className="stacks">
+                          <img
+                            className="stacksImg"
+                            src={item.source}
+                            alt=""
+                            key={item.source}
+                          ></img>
+                          <span className="name">{item.name}</span>
                         </div>
+                      ))}
+                    </div>
+
+                    <div className="links">
+                      <a href={item.github} target="_blank">
+                        <GitHub className="icon" />
+                      </a>
+                      <a href={item.live} target="_blank">
+                        <Laptop className="icon" />
+                      </a>
+                      <div className="gif" onClick={() => setGif(true)}>
+                        <GifBox className="icon" />
                       </div>
                     </div>
                   </div>
-                </>
-              ) : (
-                <>
-                  <div className="gifContainer" onClick={() => setGif(false)}>
-                    <img src={item.gif} className="gifImage" alt="" />
-                    <Close className="icon" />
-                    <span>{item.name} Site Demo</span>
-                  </div>
-                </>
-              )}
-            </div>
-          ))}
-        </div>
-        <ArrowBackIos
-          className="arrow backward"
-          onClick={() => handleClick("left")}
-        />
-        <ArrowForwardIos
-          className="arrow forward"
-          onClick={() => handleClick("right")}
-        />
-      </Desktop>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="gifContainer" onClick={() => setGif(false)}>
+                  <img src={item.gif} className="gifImage" alt="" />
+                  <Close className="icon" />
+                  <span>{item.name} Site Demo</span>
+                </div>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+      <ArrowBackIos
+        className="arrow backward"
+        onClick={() => handleClick("left")}
+      />
+      <ArrowForwardIos
+        className="arrow forward"
+        onClick={() => handleClick("right")}
+      />
     </div>
   );
 }
--- src/components/references/References.jsx
+++ src/components/references/References.jsx
@@ -1,7 +1,8 @@
 import "./references.scss";
-import { LinkedIn, AssuredWorkload } from "@mui/icons-material";
+import { LinkedIn, AssuredWorkload, Close } from "@mui/icons-material";
 import { references } from "../../data";
 import { useMediaQuery } from "react-responsive";
+import { useState } from "react";
 
 export default function References() {
   const Mobile = ({ children }) => {
@@ -9,15 +10,90 @@ export default function References() {
     return isMobile ? children : null;
   };
 
-  const Desktop = ({ children }) => {
-    const isDesktop = useMediaQuery({ minWidth: 768 });
-    return isDesktop ? children : null;
-  };
+  const [fullscreen, setFullscreen] = useState({
+    clicked: false,
+    id: "",
+    name: "",
+    title: "",
+    img: "a",
+    linkedin: "",
+    companyName: "",
+    company: "",
+    location: "",
+    desc: "",
+  });
+
+  console.log(fullscreen);
 
   const data = references;
   return (
     <div className="references" id="references">
+      <Mobile>
+        {!fullscreen.clicked ? (
+          <>
+            <h1 className="headingM">References</h1>
+            <div className="containerM">
+              {data.map((item) => (
+                <div
+                  className="cardM"
+                  onClick={() =>
+                    setFullscreen({
+                      clicked: true,
+                      id: item.id,
+                      name: item.name,
+                      title: item.title,
+                      img: item.img,
+                      linkedin: item.linkedin,
+                      companyName: item.companyName,
+                      company: item.company,
+                      location: item.location,
+                      desc: item.desc,
+                    })
+                  }
+                >
+                  <img src={item.img} alt="" />
+                  <h2>{item.name}...</h2>
+                </div>
+              ))}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="containerMFS">
+              <div className="topMFS">
+                <img className="companyMFS" src={fullscreen.company} alt="" />
+                <img className="userMFS" src={fullscreen.img} alt="" />
+                {fullscreen.id !== 3 ? (
+                  <>
+                    <a href={fullscreen.linkedin} target="_blank">
+                      <LinkedIn className="icon" />
+                    </a>
+                  </>
+                ) : (
+                  <>
+                    <a href={fullscreen.linkedin} target="_blank">
+                      <AssuredWorkload className="icon" />
+                    </a>
+                  </>
+                )}
+              </div>
+              <div className="centerMFS">{fullscreen.desc}</div>
+              <div className="bottomMFS">
+                <h3>{fullscreen.name}</h3>
+                <h4 className="locationMFS">{fullscreen.location}</h4>
+                <h4>{fullscreen.title}</h4>
+                <h4>{fullscreen.companyName}</h4>
+              </div>
+            </div>
+            <Close
+              className="iconMFS"
+              onClick={() => setFullscreen({ clicked: false })}
+            />
+          </>
+        )}
+      </Mobile>
       <h1>References</h1>
+
       <div className="container">
         {data.map((d) => (
           <div className={d.featured ? "card featured" : "card"}>
